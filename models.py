8c2577ffe7e6a5f73dbfee01b75b8c708884e338
Shaddy <shadybett540@gmail.com>
2024-01-23 00:07:56 +0300

    made student table

--- models.py
+++ models.py
@@ -6,4 +6,8 @@ class Book(db.Model):
     id = db.Column(db.Integer, Primary_key=True)
     name = db.Column(db.String(100),nullable=False)
     pages = db.Column(db.Integer,nullable=False)
-    
+
+class Student(db.Model):
+    id = db.Column(db.Integer,Primary_key=True)
+    name = db.Column(db.String(100),nullable=False)
+    age = db.Column(db.Integer,nullable=False)    
