6db963ba44c9bbf95018e7eac285959e677e629f
Shaddy <shadybett540@gmail.com>
2024-01-23 00:05:05 +0300

    made name table

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Book(db.Model):
+    id = db.Column(db.Integer, Primary_key=True)
+    name = db.Column(db.String(100),nullable=False)
+    
