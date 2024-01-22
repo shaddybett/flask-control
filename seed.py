79261ca217d41008db8b3d5115421f3711d18fbc
Shaddy <shadybett540@gmail.com>
2024-01-23 00:16:46 +0300

    fixed a type error

--- seed.py
+++ seed.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from models import db,Book,Student
-from flask_restful import Api
+from app import app
 
 app = Flask(__name__)
-api=Api(app)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///control.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db.init_app(app)
