95d7d6d8f8dc711adac8dbda1a686765ed675e29
Shaddy <shadybett540@gmail.com>
2024-01-23 00:15:28 +0300

    made sessions

--- seed.py
+++ seed.py
@@ -6,3 +6,22 @@ app = Flask(__name__)
 api=Api(app)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///control.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db.init_app(app)
+
+with app.app_context():
+    book_data={
+        'name':'SCience',
+        'pages':240
+    }
+    student_data={
+        'name':'Bett',
+        'age':18
+    }
+
+    new_book=Book(**book_data)
+    new_student=Student(**student_data)
+
+with app.db.begin():
+    db.session.add(new_book)
+    db.session.add(new_student)
+    db.session.commit()    
