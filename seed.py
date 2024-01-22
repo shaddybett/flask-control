f442e9c90eefa83bbb9d221fd23238b9de2529d1
Shaddy <shadybett540@gmail.com>
2024-01-23 00:10:50 +0300

    defined api

--- seed.py
+++ seed.py
@@ -1,4 +1,8 @@
 from flask import Flask
 from models import db,Book,Student
+from flask_restful import Api
 
 app = Flask(__name__)
+api=Api(app)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///control.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
