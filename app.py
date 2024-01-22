ba4efcf6b43cac4f2cd3183e5bcb98ce7c8747ad
Shaddy <shadybett540@gmail.com>
2024-01-22 23:56:32 +0300

    imported db from models

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_restful import Resource,Api
+from models import db
 
 app=Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///control.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db.init_app(app)
