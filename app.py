f4c6c7fe72abb3e81800d8e2aa3006bae8204c9e
Shaddy <shadybett540@gmail.com>
2024-01-23 09:34:19 +0300

    done

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_restful import Resource,Api
 from models import db,Book,Student
+from flask_migrate import Migrate
 
 app=Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///control.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db.init_app(app)
-
+migrate = Migrate(app,db)
 class BookData(Resource):
     def get(self):
        books = Book.query.all()
-       return [book.__dict__ for book in books ]
+       return jsonify([book.__dict__ for book in books ])
     
 class StudentData(Resource):
     def get(self):
